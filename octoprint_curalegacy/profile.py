397d8759b82d1dd7b17a59c73023343377c4532e
Mark Walker <markwal@hotmail.com>
2020-09-10 00:20:19 +0100

    Python 3 renames ConfigParser module to configparser
    This try/except adds support for Python 3 while maintaining Python 2
    compat.

--- octoprint_curalegacy/profile.py
+++ octoprint_curalegacy/profile.py
@@ -371,8 +371,12 @@ class Profile(object):
 			logger.warn("Path {path} does not exist or is not a file, cannot import".format(**locals()))
 			return None
 
-		import ConfigParser
-		config = ConfigParser.ConfigParser()
+		try:
+			import configparser
+		except ImportError:
+			import ConfigParser as configparser
+		config = configparser.ConfigParser()
+
 		try:
 			config.read(path)
 		except:
